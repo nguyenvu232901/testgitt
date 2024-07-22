66718c515e77b2c6b94891d9cf6e89015090b612
nguyenvu232901 <nguyentuanvu2002hn@gmail.com>
2024-07-22 15:02:16 +0700

    22/7

---
+++ source/repos/BTL_WebNC_WebQuanLyLopHocTrucTuyen/BTL_WebNC_WebQuanLyLopHocTrucTuyen/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BTL_WebNC_WebQuanLyLopHocTrucTuyen.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
